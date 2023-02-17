180ba6e12069af3fdba5a5f3192e02261cd9258e
Ana Clara <105654178+AnjaClara@users.noreply.github.com>
2023-02-17 17:20:08 -0300

    Add files via upload

---
+++ slide5Ex1.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+
+    
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int x[] = {1,2,3,4,5};
+    
+    
+    printf("O item do array na posicao 1 eh: %d", x[1]);
+    
+    return 0;
+}
+
+
+
+
+
+
